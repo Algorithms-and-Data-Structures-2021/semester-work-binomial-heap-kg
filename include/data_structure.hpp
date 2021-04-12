5e732b6a82c5f5299b091d14d03b5dff574bc1d5
Elena Gordeeva <gordeewa29@gmail.com>
2021-04-12 18:28:30 +0300

    Add implementation of BinomialHeapMinimum method

--- include/data_structure.hpp
+++ include/data_structure.hpp
@@ -15,11 +15,29 @@ namespace itis {
   struct BinomialHeap {
    public:
     int size_{0};
-    int capacity_{0};
+    int capacity_;
     int* data_{nullptr};
+    Node *head_{nullptr};
 
     // Tip 2: На начальном этапе разработки структуры данных можете определения методов задавать в
     // заголовочном файле, как только работа будет завершена, можно будет оставить здесь только объявления.
+
+    explicit BinomialHeap(int capacity) : capacity_{capacity} {};
+
+    Node *BinomialHeapMinimum(){     // Compare to 0(lg n)
+      Node *y = nullptr;
+      Node *x = head_;
+      int min = 0;
+      min = std::numeric_limits<int>::max();
+      while (x != nullptr){
+        if (x->key < min) {
+          min = x->key;
+          y = x;
+        }
+        x = x->sibling;
+      }
+      return y;
+    }
 
     int size() const {
       return size_;
