a274ce341a3471d12c9b89039f2f231dce8280d0
Varvara Korneeva <varvara_korneeva@mail.ru>
2021-04-23 18:22:20 +0300

    cpp

--- include/data_structure.hpp
+++ include/data_structure.hpp
@@ -15,174 +15,36 @@ namespace itis {
     int data, degree;
     Node *child, *sibling, *parent;
 
-    static Node *newNode(int key) {
-      Node *temp = new Node;
-      temp->data = key;
-      temp->degree = 0;
-      temp->child = temp->parent = temp->sibling = nullptr;
-      return temp;
-    }
+    static Node *newNode(int key);
   };
 
   // Пример: объявление структуры с полями и методами
-  struct MyStructure {
+  struct BinomialHeap {
    public:
     int size_{0};
 //    int capacity_{0};
     int* data_{nullptr};
     std::list<Node*> root_list;
 
-    Node* mergeBinomialTrees(Node *b1, Node *b2) {
-      if (b1->data > b2->data) {
-        std::swap(b1, b2);
+    Node* mergeBinomialTrees(Node *b1, Node *b2);
 
-        b2->parent = b1;
-        b2->sibling = b1->child;
-        b1->child = b2;
-        b1->degree++;
-      }
-      return b1;
-    }
+    std::list<Node*> unionBinomialHeap(std::list<Node*> l1, std::list<Node*> l2);
 
-    std::list<Node*> unionBinomialHeap(std::list<Node*> l1, std::list<Node*> l2){
-      std::list<Node*> _new;
-      std::list<Node*>::iterator i1 = l1.begin();
-      std::list<Node*>::iterator i2 = l2.begin();
-      while(i1 != l1.end() && i2 != l2.end()){
-        if((*i1)->degree <= (*i2)->degree){
-          _new.push_back(*i1);
-          i1++;
-        }
-        else{
-          _new.push_back(*i2);
-          i2++;
-        }
-      }
-      while(i1 != l1.end()){
-        _new.push_back(*i1);
-        i1++;
-      }
-      while(i2 != l2.end()){
-        _new.push_back(*i2);
-        i2++;
-      }
-      return _new;
-    }
+    std::list<Node*> adjust(std::list<Node*> _heap);
 
-    std::list<Node*> adjust(std::list<Node*> _heap) {
-      if (_heap.size() <= 1)
-        return _heap;
-      std::list<Node*> new_heap;
-      std::list<Node*>::iterator it1, it2, it3;
-      it1 = it2 = it3 = _heap.begin();
+    std::list<Node*> insertATreeInHeap(std::list<Node*> _heap, Node *tree);
 
-      if (_heap.size() == 2) {
-        it2 = it2;
-        it2++;
-        it3 = _heap.end();
-      } else {
-        it2++;
-        it3 = it2;
-        it3++;
-      }
-      while (it1 != _heap.end()) {
-        if (it2 == _heap.end())
-          it1++;
-        else if ((*it1)->degree < (*it2)->degree) {
-          it1++;
-          it2++;
-          if (it3 != _heap.end())
-            it3++;
-        }
-        else if (it3 != _heap.end() &&
-            (*it1)->degree == (*it2)->degree &&
-            (*it1)->degree == (*it3)->degree) {
-          it1++;
-          it2++;
-          it3++;
-        }
-        else if ((*it1)->degree == (*it2)->degree) {
-//          Node *temp;  // can it be deleted?
-          *it1 = mergeBinomialTrees(*it1, *it2);
-          it2 = _heap.erase(it2);
-          if (it3 != _heap.end())
-            it3++;
-        }
-      }
-      return _heap;
-    }
-    std::list<Node*> insertATreeInHeap(std::list<Node*> _heap, Node *tree){
-      std::list<Node*> temp;
-      temp.push_back(tree);
-      temp = unionBinomialHeap(_heap, temp);
-      return adjust(temp);
-    }
+    std::list<Node*> insert(std::list<Node*> _heap, int key);
 
-    std::list<Node*> insert(std::list<Node*> _heap, int key){
-      Node *temp = Node::newNode(key);
-      return insertATreeInHeap(_heap, temp);
-    }
+    Node* getMin(std::list<Node*> _heap);
 
-    Node* getMin(std::list<Node*> _heap) {
-      std::list<Node*>::iterator it = _heap.begin();
-      Node *temp = *it;
-      while (it != _heap.end()) {
-        if ((*it)->data < temp->data)
-          temp = *it;
-        it++;
-      }
-      return temp;
-    }
+    std::list<Node*> extractMin(std::list<Node*> _heap);
 
-    std::list<Node*> extractMin(std::list<Node*> _heap){
-      std::list<Node*> new_heap, lo;
-      Node *temp;
+    std::list<Node*> removeMinFromTreeReturnBHeap(Node *tree);
 
-      temp = getMin(_heap);
-      std::list<Node*>::iterator i;
-      i = _heap.begin();
-      while (i != _heap.end()){
-        if(*i != temp){
-          new_heap.push_back(*i);
-        }
-        i++;
-      }
-      lo = removeMinFromTreeReturnBHeap(temp);
-      new_heap = unionBinomialHeap(new_heap, lo);
-      new_heap = adjust(new_heap);
-      return new_heap;
-    }
+    void printTree(Node *h);
 
-    std::list<Node*> removeMinFromTreeReturnBHeap(Node *tree) {
-      std::list<Node*> heap;
-      Node *temp = tree->child;
-      Node *lo;
-
-      while (temp) {
-        lo = temp;
-        temp = temp->sibling;
-        lo->sibling = nullptr;
-        heap.push_front(lo);
-      }
-      return heap;
-    }
-
-    void printTree(Node *h) {
-      while (h) {
-        std::cout << h->data << " ";
-        printTree(h->child);
-        h = h->sibling;
-      }
-    }
-
-    void printHeap(std::list<Node*> _heap) {
-      std::list<Node*>::iterator  it;
-      it = _heap.begin();
-      while (it != _heap.begin()) {
-        printTree(*it);
-        it++;
-      }
-    }
+    void printHeap(std::list<Node*> _heap);
     // Tip 2: На начальном этапе разработки структуры данных можете определения методов задавать в
     // заголовочном файле, как только работа будет завершена, можно будет оставить здесь только объявления.
 
--- src/data_structure.cpp
+++ src/data_structure.cpp
@@ -5,5 +5,155 @@
 namespace itis {
 
   // здесь должны быть определения методов вашей структуры
+  Node *Node::newNode(int key) {
+    Node *temp = new Node;
+    temp->data = key;
+    temp->degree = 0;
+    temp->child = temp->parent = temp->sibling = nullptr;
+    return temp;
+  }
 
+  Node *BinomialHeap::mergeBinomialTrees(Node *b1, Node *b2) {
+    if (b1->data > b2->data) {
+      std::swap(b1, b2);
+
+      b2->parent = b1;
+      b2->sibling = b1->child;
+      b1->child = b2;
+      b1->degree++;
+    }
+    return b1;
+  }
+  std::list<Node *> BinomialHeap::unionBinomialHeap(std::list<Node *> l1, std::list<Node *> l2) {
+    std::list<Node*> _new;
+    std::list<Node*>::iterator i1 = l1.begin();
+    std::list<Node*>::iterator i2 = l2.begin();
+    while(i1 != l1.end() && i2 != l2.end()){
+      if((*i1)->degree <= (*i2)->degree){
+        _new.push_back(*i1);
+        i1++;
+      }
+      else{
+        _new.push_back(*i2);
+        i2++;
+      }
+    }
+    while(i1 != l1.end()){
+      _new.push_back(*i1);
+      i1++;
+    }
+    while(i2 != l2.end()){
+      _new.push_back(*i2);
+      i2++;
+    }
+    return _new;
+  }
+  std::list<Node *> BinomialHeap::adjust(std::list<Node *> _heap) {
+    if (_heap.size() <= 1)
+      return _heap;
+    std::list<Node*> new_heap;
+    std::list<Node*>::iterator it1, it2, it3;
+    it1 = it2 = it3 = _heap.begin();
+
+    if (_heap.size() == 2) {
+      it2 = it2;
+      it2++;
+      it3 = _heap.end();
+    } else {
+      it2++;
+      it3 = it2;
+      it3++;
+    }
+    while (it1 != _heap.end()) {
+      if (it2 == _heap.end())
+        it1++;
+      else if ((*it1)->degree < (*it2)->degree) {
+        it1++;
+        it2++;
+        if (it3 != _heap.end())
+          it3++;
+      }
+      else if (it3 != _heap.end() &&
+               (*it1)->degree == (*it2)->degree &&
+               (*it1)->degree == (*it3)->degree) {
+        it1++;
+        it2++;
+        it3++;
+      }
+      else if ((*it1)->degree == (*it2)->degree) {
+//          Node *temp;  // can it be deleted?
+        *it1 = mergeBinomialTrees(*it1, *it2);
+        it2 = _heap.erase(it2);
+        if (it3 != _heap.end())
+          it3++;
+      }
+    }
+    return _heap;
+  }
+  std::list<Node *> BinomialHeap::insertATreeInHeap(std::list<Node *> _heap, Node *tree) {
+    std::list<Node*> temp;
+    temp.push_back(tree);
+    temp = unionBinomialHeap(_heap, temp);
+    return adjust(temp);
+  }
+  std::list<Node *> BinomialHeap::insert(std::list<Node *> _heap, int key) {
+    Node *temp = Node::newNode(key);
+    return insertATreeInHeap(_heap, temp);
+  }
+  Node *BinomialHeap::getMin(std::list<Node *> _heap) {
+    std::list<Node*>::iterator it = _heap.begin();
+    Node *temp = *it;
+    while (it != _heap.end()) {
+      if ((*it)->data < temp->data)
+        temp = *it;
+      it++;
+    }
+    return temp;
+  }
+  std::list<Node *> BinomialHeap::extractMin(std::list<Node *> _heap) {
+    std::list<Node*> new_heap, lo;
+    Node *temp;
+
+    temp = getMin(_heap);
+    std::list<Node*>::iterator i;
+    i = _heap.begin();
+    while (i != _heap.end()){
+      if(*i != temp){
+        new_heap.push_back(*i);
+      }
+      i++;
+    }
+    lo = removeMinFromTreeReturnBHeap(temp);
+    new_heap = unionBinomialHeap(new_heap, lo);
+    new_heap = adjust(new_heap);
+    return new_heap;
+  }
+  std::list<Node *> BinomialHeap::removeMinFromTreeReturnBHeap(Node *tree) {
+    std::list<Node*> heap;
+    Node *temp = tree->child;
+    Node *lo;
+
+    while (temp) {
+      lo = temp;
+      temp = temp->sibling;
+      lo->sibling = nullptr;
+      heap.push_front(lo);
+    }
+    return heap;
+  }
+  void BinomialHeap::printTree(Node *h) {
+    while (h) {
+      std::cout << h->data << " ";
+      printTree(h->child);
+      h = h->sibling;
+    }
+  }
+  void BinomialHeap::printHeap(std::list<Node *> _heap) {
+    std::list<Node*>::iterator  it;
+    it = _heap.begin();
+    while (it != _heap.begin()) {
+      printTree(*it);
+      it++;
+    }
+  }
 }  // namespace itis
