e8707f955f4e58932db1ce3808f6a7a81ac5602c
Varvara Korneeva <varvara_korneeva@mail.ru>
2021-04-24 20:20:42 +0300

    csv writing

--- benchmark/demo_benchmark.cpp
+++ benchmark/demo_benchmark.cpp
@@ -16,6 +16,31 @@ using namespace itis;
 static constexpr auto kDatasetPath = string_view{PROJECT_DATASET_DIR};
 static constexpr auto kProjectPath = string_view{PROJECT_SOURCE_DIR};
 
+itis::BinomialHeap* makeHeap(string path){
+  auto input_file = ifstream(path);
+  auto *heap = new itis::BinomialHeap();
+  if (input_file) {
+//    vector<string> data;
+//    auto *heap = new itis::BinomialHeap();
+    std::string line;
+//    int i = 0;
+    while (std::getline(input_file, line)){
+  //      cout << line << endl;
+      std::istringstream s(line);
+      std::string field;
+      while (std::getline(s, field, ',')) {
+        heap = heap->insert(heap, stoi(field));
+//        data.push_back(field);
+//        cout << field << endl;
+  //        cout << data[i] << endl;
+//        i++;
+      }
+    }
+  }
+  heap->printHeap(heap);
+  return heap;
+}
+
 int main(int argc, char **argv) {
 
   // Tip 1: входные аргументы позволяют более гибко контролировать параметры вашей программы
@@ -37,28 +62,49 @@ int main(int argc, char **argv) {
   ss >> number;  // number = 1
   ss >> number;  // number = 2
 
+  vector<string> mas{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+  vector<string> mas2{"100", "500", "1000", "5000", "750000", "1000000", "2500000", "5000000"};
+  vector<string> mas3{"01", "02", "03", "04", "05", "06", "07", "08", "09", "10"};
+
   // работа с набором данных
   const auto path = string(kDatasetPath);
   cout << "Path to the 'dataset/' folder: " << path << endl;
 
-  auto input_file = ifstream(path + "/dataset-example.csv");
-
-  if (input_file) {
-    // чтение и обработка набора данных ...
-    vector<string> data;
-    std::string line;
-    int i = 0;
-    while (std::getline(input_file, line)){
-      std::istringstream s(line);
-      std::string field;
-      while (std::getline(s, field, ',')){
-        data.push_back(field);
-//        cout << field << endl;
-        cout << data[i] << endl;
-        i++;
+  auto output_file = ofstream(path);
+//  ofstream output_file;
+  output_file.open(path + "/" + "result.csv", ios::out | ios::app);
+  output_file << "file" << "," << "count" << "," << "number" << "," << "insert" << "," << "get_min" << "," << "extract_min" << endl;
+  for(int i =0; i < 8; i++){
+    for(int j = 0; j < 10; j++){
+      auto part_file = string(mas3.at(j) + mas2.at(i));
+      for(int k = 0; k < 10; k++){
+//        auto input_file = ifstream(path + part_file);
+        output_file << mas3.at(j) << "," << mas2.at(i) << "," << mas.at(k) << endl;
       }
     }
   }
+
+  makeHeap(path + "/insert/01/100.csv");
+
+//  auto input_file = ifstream(path + "/insert/01/100.csv");
+//
+//  if (input_file) {
+//    // чтение и обработка набора данных ...
+//    vector<string> data;
+//    std::string line;
+//    int i = 0;
+//    while (std::getline(input_file, line)){
+////      cout << line << endl;
+//      std::istringstream s(line);
+//      std::string field;
+//      while (std::getline(s, field, ',')) {
+//        data.push_back(field);
+//        cout << field << endl;
+////        cout << data[i] << endl;
+//        i++;
+//      }
+//    }
+//  }
 
   // Контрольный тест: операции добавления, удаления, поиска и пр. над структурой данных
 
@@ -73,9 +119,9 @@ int main(int argc, char **argv) {
 
   // переводим время в наносекунды
   const auto time_diff = time_point_after - time_point_before;
-  const long time_elapsed_ns = chrono::duration_cast<chrono::nanoseconds>(time_diff).count();
+//  const long time_elapsed_ns = chrono::duration_cast<chrono::nanoseconds>(time_diff).count();
 
-  cout << "Time elapsed (ns): " << time_elapsed_ns << '\n';
+//  cout << "Time elapsed (ns): " << time_elapsed_ns << '\n';
 
   return 0;
 }
